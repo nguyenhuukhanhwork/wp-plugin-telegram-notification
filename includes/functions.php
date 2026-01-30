1fe4346fe9ee18d095a7c3d178f6e32fb1e08d83
Nguyễn Hữu Khánh <nguyenhuukhanhwork@gmail.com>
2026-01-31 01:09:57 +0700

    Add Facade
    telegram_send($message)

---
+++ includes/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file implement functions for dev ue
+ */
+
+
+use TelegramNotification\Core\Telegram_Service;
+
+/**
+ * Function simple for send Telegram Message
+ * @param string $message
+ * @param string $chat_id
+ * @return bool
+ */
+function telegram_send( string $message, string $chat_id = '') {
+
+    // Check Chat Id is string
+    if ( ! is_string( $chat_id ) ) {
+        error_log('telegram_send $message is string');
+        return false;
+    }
+
+    // Check empty $message
+    $message = trim( $message );
+    if ( $message === '' ) {
+        return false;
+    }
+
+    // Send Message
+    return Telegram_Service::send( $message, $chat_id );
+}
